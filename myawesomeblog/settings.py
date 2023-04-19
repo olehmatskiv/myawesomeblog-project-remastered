5f02d252980d2149df79f5f18c0525201fb30212
Oleh <85799629+olehmatskiv@users.noreply.github.com>
2023-04-19 14:51:55 +0300

    add static files

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -121,7 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'myawesomeblog/static/'
+]
+STATIC_ROOT = BASE_DIR / 'static'
+STATIC_URL = '/static/'
+
+
 MEDIA_ROOT = BASE_DIR / 'media'
 MEDIA_URL = '/meida/'
 # Default primary key field type
